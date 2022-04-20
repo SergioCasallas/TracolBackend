09900117706ba4d7835ddb71d87a4074bce99d82
SergioCasallas <sergiocasallas01@gmail.com>
2022-04-20 12:09:06 -0500

    Change Query Actas

--- query/datosActa_query/datosActa_query.js
+++ query/datosActa_query/datosActa_query.js
@@ -1,8 +1,19 @@
-exports.getDatosActa=(numeroTRA, codigoVerificacion)=>{ 
-console.log(numeroTRA)
-
-return `
-SELECT ip_CRM_Col.Tracol_Service_request_Details.Collection_Order, ip_CRM_Col.Tracol_Service_request_Details.Company_Name, ip_CRM_Col.Tracol_Service_request_Details.Company_Address, ip_CRM_Col.Tracol_Service_request_Details.Collection_Date, ip_CRM_Col.Tracol_Service_request_Details.UUID_ACTA FROM ip_CRM_Col.Tracol_Service_request_Details where ip_CRM_Col.Tracol_Service_request_Details.Collection_Order = '${numeroTRA}' and ip_CRM_Col.Tracol_Service_request_Details.UUID_ACTA = '${codigoVerificacion}'
+exports.getDatosActa = (numeroTRA, codigoVerificacion) => {
+  return `
+SELECT ip_CRM_Col.Tracol_Service_request_Details.Residue, ip_CRM_Col.Tracol_Service_request_Details.Weight, ip_CRM_Col.Tracol_Service_request_Details.Quantity , ip_CRM_Col.Tracol_Service_request_Details.Residue_Type , ip_CRM_Col.Tracol_Service_request_Details.Tipo_pretratamiento, ip_CRM_Col.Tracol_Service_request_Details.Gestor, ip_CRM_Col.Tracol_Service_request_Details.Package, ip_CRM_Col.Tracol_Service_request_Details.Collection_Order, ip_CRM_Col.Tracol_Service_request_Details.Company_Name, ip_CRM_Col.Tracol_Service_request_Details.Company_Address, ip_CRM_Col.Tracol_Service_request_Details.Collection_Date, ip_CRM_Col.Tracol_Service_request_Details.UUID_ACTA FROM ip_CRM_Col.Tracol_Service_request_Details where ip_CRM_Col.Tracol_Service_request_Details.Collection_Order = '${numeroTRA}' and ip_CRM_Col.Tracol_Service_request_Details.UUID_ACTA = '${codigoVerificacion}'
     `;
 };
 
+// numerotra
+// gestor
+// fecha
+
+// datos especificos
+
+// cliente
+// sede
+// residuo
+// peso
+// embalaje
+// tipo de residuo
+// prepratratamiento
